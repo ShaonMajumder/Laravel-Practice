6f945b512ad2bb99d91b183663c1ac6c97dda422
Shaon Majumder <smazoomder@gmail.com>
2020-11-21 17:28:54 +0600

    directives

---
+++ blog/resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+@extends('layout')
+@section('content')
+<h1>This is the contact page.</h1>
+@endsection
--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Shaon's Blog</title>
-</head>
-<body>
-    <h1>Welcome to my laravel project.</h1>
-</body>
-</html>
+@extends('layout')
+@section('content')
+<h1>Welcome to my laravel project. This is the home page.</h1>
+@endsection
