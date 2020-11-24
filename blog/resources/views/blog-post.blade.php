06dc454446de83813141c4d40cbb200e038f65db
Shaon Majumder <smazoomder@gmail.com>
2020-11-24 18:07:42 +0600

    querying

--- blog/resources/views/blog-post.blade.php
+++ blog/resources/views/blog-post.blade.php
@@ -1,4 +1,10 @@
 @extends('layout')
 @section('content')
     {!! $wel !!}{{ $data['tid'] }}
+    {!!
+        $posts = App\BlogPost::all(); 
+        foreach($posts as $post){
+            echo "{$post->title}\n";
+        }
+    !!}
 @endsection
